422e5b2beebd0fc2bd5f72008c5b12e9bbc5a095
Hiroya <soten.bluesky@gmail.com>
2019-01-27 19:09:45 +0900

    reset https://qiita.com/expajp/items/fe50fb57ccc7e43d6291

---
+++ Backlog記法をMarkdownへ変換する/Backlog記法をMarkdownへ変換する.js
@@ -0,0 +1,398 @@
+((textarea, translate) => {
+    'use strict';
+
+    if (!textarea) {
+        throw new TypeError('テキストエリアが見つかりませんでした。');
+    }
+
+    textarea.value = translate(textarea.value);
+})(document.querySelector('#descriptionTextArea, [id="page.content"]'), (val) => {
+    'use strict';
+
+    const codes = [];
+    const quotes = [];
+    const paragraphs = [];
+    const textLevelSemanticsCheck = (content) => {
+        const patterns = [
+            // 要素を<tagName>表記しているパターンをエスケープ
+            {
+                pattern: /<(.*?)>/g,
+                replacement: (m, p1) => {
+                    return `&lt;${p1}&gt;`;
+                }
+            },
+
+            // 抜け漏れしている<をエスケープ。>は他の記法で利用されているためエスケープしない
+            {
+                pattern: /</g,
+                replacement: (m, p1) => {
+                    return '&lt;';
+                }
+            },
+
+            // strong
+            {
+                pattern: /''(.*?)''/g,
+                replacement: (m, p1) => {
+                    return ` **${p1.trim()}** `;
+                }
+            },
+
+            // em
+            {
+                pattern: /'(.*?)'/g,
+                replacement: (m, p1) => {
+                    return ` *${p1.trim()}* `;
+                }
+            },
+
+            // strike
+            {
+                pattern: /%%(.*?)%%/g,
+                replacement: (m, p1) => {
+                    return ` ~~${p1.trim()}~~ `;
+                }
+            },
+
+            // a
+            {
+                pattern: /\[\[(.*?)[:>](.*?)\]\]/g,
+                replacement: (m, p1, p2) => {
+                    return `[${p1.trim()}](${p2})`;
+                }
+            },
+
+            // 色指定（Markdown in Backlogでは無視される）
+            {
+                pattern: /&color\((.*?)\)(\s+)?{(.*?)}/ig,
+                replacement: (m, p1, p2, p3) => {
+                    return `<span style="color: ${p1};">${p3}</span>`;
+                }
+            },
+
+            // その他埋め込み
+            {
+                pattern: /#image\((.*?)\)/,
+                replacement: (m, p1) => {
+                    return `![${p1}]`;
+                }
+            },
+            {
+                pattern: /#thumbnail\((.*?)\)/,
+                replacement: (m, p1) => {
+                    return `![${p1}]`;
+                }
+            },
+            {
+                pattern: /#attach\((.*?):(.*?)\)/,
+                replacement: (m, p1, p2) => {
+                    return `[${p1}][${p2}]`;
+                }
+            },
+
+            // 余計な半角スペースの連続を削除
+            {
+                pattern: /  /,
+                replacement: ' '
+            },
+
+            {
+                pattern: /&/,
+                replacement: '&amp;'
+            }
+        ];
+
+        patterns.forEach(({pattern, replacement}) => {
+            content = content.replace(pattern, replacement);
+        });
+
+        return content;
+    };
+    const patterns = [
+        // 改行コード
+        {
+            pattern: /\n\r/g,
+            replacement: '\n'
+        },
+        {
+            pattern: /\r/g,
+            replacement: '\n'
+        },
+
+
+
+
+        // 見出し
+        {
+            pattern: /^(\*+)(.*)$/gm,
+            replacement: (m, p1, p2) => {
+                return `\n${p1.replace(/\*/g, '#')} ${textLevelSemanticsCheck(p2.trim())}\n`;
+            }
+        },
+
+
+
+
+        // theadなしテーブル
+        {
+            pattern: /\n\|([\s|\S]*)\|\n(?!\|)/g,
+            replacement: (m, p1) => {
+                if (p1.indexOf('|h\n') === -1) {
+                    let i = 0;
+                    let max = p1.split('\n')[0].split('|').length;
+                    let row = '';
+
+                    for (i; i < max; i++) {
+                        row += '|:--';
+                    }
+
+                    row += '|';
+
+
+                    return `\n${row}\n|${p1}|\n`;
+                }
+
+                return `\n|${p1}|\n`;
+            }
+        },
+
+
+
+
+        // 行見出しテーブル
+        {
+            pattern: /^\|(.*)\|(\s?)$/gm,
+            replacement: (m, p1, p2) => {
+                p1 = p1.replace(/\|~/g, '|');
+                p1 = p1.replace(/^~/g, '');
+                p1 = p1.replace(/\|\|/g, '| |');
+                p1 = p1.replace(/^\|/g, ' |');
+                p1 = p1.replace(/^\|/g, ' |'); // 先頭が空
+                p1 = p1.replace(/\|$/g, '| '); // 最後が空
+
+                return `|${p1}|${p2}`;
+            }
+        },
+        // 列見出しテーブル
+        {
+            pattern: /^\|(.*)\|h\s?$/gm,
+            replacement: (m, p1) => {
+                let row = '';
+                let cell = p1.split('|');
+                let i = 0;
+                let max = cell.length;
+
+                for (i; i < max; i++) {
+                    row += '|:--';
+                }
+
+                row += '|';
+
+                p1 = p1.replace(/\|~/g, '|');
+                p1 = p1.replace(/^~/g, '');
+                p1 = p1.replace(/\|\|/g, '| |');
+                p1 = p1.replace(/^\|/g, ' |'); // 先頭が空
+                p1 = p1.replace(/\|$/g, '| '); // 最後が空
+
+                return `\n|${p1}|\n${row}`;
+            }
+        },
+        // テーブルに改行
+        {
+            pattern: /\n\|([\s|\S]*?)\|\n([^|])/g,
+            replacement: (m, p1, p2) => {
+                return `\n|${textLevelSemanticsCheck(p1)}|\n\n${p2}`
+            }
+        },
+        {
+            pattern: /\n\|([\s|\S]*?)\|\n(?!\|)/g,
+            replacement: (m, p1) => {
+                return `\n\n|${p1}|\n\n`
+            }
+        },
+
+
+
+
+        // 順序リスト
+        {
+            pattern: /\n\+([\s|\S]*?)\n\n/g,
+            replacement: (m, p1) => {
+                return `\n+${p1}\n\n\n`;
+            }
+        },
+        {
+            pattern: /\n\+([\s|\S]*?)\n\n/g,
+            replacement: (m, p1) => {
+                let result = '';
+
+                p1 = '\n+' + p1.trim();
+                // スペースの整形
+                p1 = p1.replace(/^(\++)(.*)$/gm, (m, p1, p2) => {
+                    return `${p1} ${p2.trim()}`;
+                });
+                p1 = p1.trim();
+
+                {
+                    const symbolCount = []; // index番号はインデントレベル。インデントが上がったら
+                    let currentLevel = 0;
+
+                    p1.split('\n').forEach((line) => {
+                        const level = line.split(' ')[0].length;
+
+                        if (level < currentLevel) {
+                            symbolCount[currentLevel] = 0;
+                        }
+
+                        currentLevel = level;
+                        symbolCount[currentLevel] = symbolCount[currentLevel] ? symbolCount[currentLevel] + 1 : 1;
+                        result += textLevelSemanticsCheck(line.replace('+ ', symbolCount[currentLevel] + '. ')) + '\n';
+                    });
+                }
+
+
+                // ネストがあるときに残っている + をスペースに変換
+                p1 = result.replace(/^(\++)(.*)/gm, (m, p1, p2) => {
+                    const max = p1.length;
+                    let i = 0;
+                    let indent = '';
+
+                    for (i; i < max; i++) {
+                        indent += '    ';
+                    }
+
+                    return indent + p2;
+                });
+
+
+                return `\n${p1}\n`;
+            }
+        },
+
+
+
+
+        // 非順序リスト
+        {
+            pattern: /^(-+)(.*)$/gm,
+            replacement: (m, p1, p2) => {
+                let i = 0;
+                let max = p1.length - 1;
+                let indent = '';
+
+                if (!p2) {
+                    return p1; // hr要素
+                }
+
+                for (i; i < max; i++) {
+                    indent += '    ';
+                }
+
+                indent += '-';
+                p2 = textLevelSemanticsCheck(p2).trim();
+
+                return `${indent} ${p2}`;
+            }
+        },
+
+
+
+
+        // 改行をbr要素に変換（Markdown in Backlogでは無視されます）
+        {
+            pattern: /&br;/g,
+            replacement: ' <br>'
+        }
+    ];
+
+    // 正規表現を助けるための改行を追加
+    val = '\n' + val + '\n\n';
+
+    // 目次
+    val = val.replace(/^#contents$/gm, '[toc]\n');
+
+    // コード範囲指定を隠す
+    val = val.replace(/\n{code}([\s|\S]*?){\/code}\n/g, (m, p1) => {
+        codes.push(p1);
+
+        return `\n{{CODE_REPACE_BACKLOG_TO_MARKDOWN-${codes.length - 1}}}\n`;
+    });
+
+    // 引用範囲指定を隠す
+    val = val.replace(/\n{quote}([\s|\S]*?){\/quote}\n/g, (m, p1) => {
+        quotes.push(p1);
+
+        return `\n{{QUOTE_REPACE_BACKLOG_TO_MARKDOWN-${quotes.length - 1}}}\n`;
+    });
+
+    // 通常テキスト（パラグラフ）を隠す
+    val = val.replace(/^.*$/gm, (() => {
+        const isP = /^(?![*\|\-\+\s>)`])(.*)$/;
+
+        return (p1) => {
+            if (
+                p1 &&
+                isP.test(p1) &&
+                !p1.startsWith('{{CODE_REPACE_BACKLOG_TO_MARKDOWN') &&
+                !p1.startsWith('{{QUOTE_REPACE_BACKLOG_TO_MARKDOWN')
+            ) {
+                paragraphs.push(p1);
+
+                return `{{PARAGRAPHS_REPACE_BACKLOG_TO_MARKDOWN-${paragraphs.length - 1}}}`;
+            }
+
+            return p1;
+        }
+    })());
+    // パラグラフの塊は最後に空行を開けさせる
+    val = val.replace(/\n{{PARAGRAPHS_REPACE_BACKLOG_TO_MARKDOWN-.*?}}\n(?!{{)/g, (p1) => {
+        return `${p1}\n`;
+    });
+
+
+    // 範囲指定型以外のBacklog記法を置き換える
+    patterns.forEach(({pattern, replacement}) => {
+        val = val.replace(pattern, replacement);
+    });
+
+
+    // 範囲指定系を埋めもどす前に無駄な改行を削除する
+    while (/\n\n\n/g.test(val)) {
+        val = val.replace('\n\n\n', '\n\n');
+    }
+
+
+    // コード範囲指定を戻す
+    val = val.replace(/{{CODE_REPACE_BACKLOG_TO_MARKDOWN-(.*?)}}/g, (m, p1) => {
+        let content = codes[Number(p1)].trim();
+
+        if (content) {
+            return '\n```\n' + codes[Number(p1)].trim() + '\n```\n';
+        }
+
+        return '\n```\n```\n';
+    });
+
+
+    // 引用範囲指定を戻す
+    val = val.replace(/{{QUOTE_REPACE_BACKLOG_TO_MARKDOWN-(.*?)}}/g, (m, p1) => {
+        let content = quotes[Number(p1)].trim();
+
+        content = content.split('\n').join('\n> ');
+
+        return '\n> ' + content + '\n';
+    });
+
+
+    // パラグラフを戻す
+    val = val.replace(/{{PARAGRAPHS_REPACE_BACKLOG_TO_MARKDOWN-(.*?)}}/g, (m, p1) => {
+        let content = paragraphs[Number(p1)].trim();
+
+        content = textLevelSemanticsCheck(content);
+
+        return content;
+    });
+
+
+    return val.trim();
+});
---
+++ 複数のブックマークレットを結合する.js/複数のブックマークレットを結合する.js
@@ -0,0 +1,166 @@
+/* eslint-disable no-console, no-script-url */
+((win, doc) => {
+    'use strict';
+
+    const init = () => {
+        const CONCAT_BOOKMARKLETS = win.CONCAT_BOOKMARKLETS;
+        const shadowRoot = CONCAT_BOOKMARKLETS.attachShadow({
+            mode: 'closed'
+        });
+        const content = (() => {
+            const fragment = doc.createDocumentFragment();
+            const style = doc.createElement('style');
+            const closeBtn = doc.createElement('button');
+            const concatBtn = doc.createElement('button');
+            const resultWrap = doc.createElement('div');
+            const resultInput = doc.createElement('input');
+            const concatResponse = doc.createElement('span');
+            const tasks = {
+                close: () => {
+                    CONCAT_BOOKMARKLETS.remove();
+                },
+                getFormSet: function () {
+                    const getFormSet = doc.createDocumentFragment();
+                    const wrapper = doc.createElement('div');
+                    const titleInput = doc.createElement('input');
+                    const codeInput = doc.createElement('input');
+                    const plusBtn = doc.createElement('button');
+                    const deleteBtn = doc.createElement('button');
+
+                    titleInput.placeholder = 'タスク名';
+                    titleInput.setAttribute('aria-label', 'タスク名');
+                    codeInput.placeholder = 'javascript:';
+                    codeInput.setAttribute('aria-label', '結合したいブックマークレットのJavaScriptコード');
+                    wrapper.className = 'item';
+                    plusBtn.type = 'button';
+                    plusBtn.className = 'plus';
+                    plusBtn.innerHTML = '<span>追加</span>';
+                    plusBtn.addEventListener('click', this.add);
+                    deleteBtn.type = 'button';
+                    deleteBtn.className = 'delete';
+                    deleteBtn.innerHTML = '<span>削除</span>';
+                    deleteBtn.addEventListener('click', this.remove);
+
+                    wrapper.append(titleInput);
+                    wrapper.append(codeInput);
+                    wrapper.append(plusBtn);
+                    wrapper.append(deleteBtn);
+                    getFormSet.append(wrapper);
+
+                    return getFormSet;
+                },
+                add: function () {
+                    const formSet = tasks.getFormSet();
+
+                    this.parentNode.after(formSet);
+                },
+                remove: function () {
+                    const {parentNode} = this;
+                    const {previousElementSibling, nextElementSibling} = parentNode;
+
+                    if (
+                        (
+                            previousElementSibling &&
+                            previousElementSibling.className === 'item'
+                        ) ||
+                        (
+                            nextElementSibling &&
+                            nextElementSibling.className === 'item'
+                        )
+                    ) {
+                        parentNode.remove();
+                    }
+                },
+                concat: (() => {
+                    let key = null;
+                    let hide = () => {
+                        concatResponse.setAttribute('aria-hidden', 'true');
+                    };
+
+                    return () => {
+                        const itemSet = shadowRoot.querySelectorAll('.item');
+                        const srcSet = [];
+
+                        itemSet.forEach((item) => {
+                            const code = (() => {
+                                let {value} = item.children[1];
+
+                                value = value.replace(/^javascript:/, '');
+
+                                try {
+                                    return decodeURIComponent(value);
+                                } catch (e) {
+                                    return value;
+                                }
+                            })();
+                            const title = item.firstElementChild.value || code;
+
+                            srcSet.push({
+                                title: title,
+                                code: code
+                            });
+                        });
+
+                        resultInput.value = `javascript:(() => {const srcSet = ${JSON.stringify(srcSet)}; srcSet.forEach(({title, code}) => {console.log(\`\${title} を実行します\`);try {eval(code);} catch (e) {console.error(e);}});console.log('Done: 次の内容を実行しました。');console.table(srcSet);window.CONCAT_BOOKMARKLETS = window.CONCAT_BOOKMARKLETS || Object.create(null); CONCAT_BOOKMARKLETS.executed = srcSet;})();`;
+
+                        concatResponse.setAttribute('aria-hidden', 'false');
+
+                        clearTimeout(key);
+                        key = setTimeout(hide, 1000);
+                    };
+                })()
+            };
+
+            closeBtn.type = 'button';
+            closeBtn.className = 'close';
+            closeBtn.innerHTML = '<span>閉じる</span>';
+            closeBtn.addEventListener('click', tasks.close);
+            fragment.append(closeBtn);
+
+            fragment.append(style);
+            style.textContent = ':host {overflow: auto;max-width: 500px;width: 90vw;margin: auto;line-height: 1.8;font-size: 14px;display: block;position: fixed;left: 0;right: 0;top: 0;bottom: 0;z-index: 999999;background: #fff;box-shadow: 0 0 2px rgba(0, 0, 0, .4);max-height: 90vh;padding: 10px;} * {box-sizing: border-box;}.close::before,.close::after,.plus::before,.plus::after,.delete::before,.delete::after {content: ""; width: 3px;background: #aaa;display: block;left: 0;right: 0;top: 0;bottom: 0;margin: auto;z-index: 0;position: absolute;}.plus::before,.plus::after,.delete::before,.delete::after {height: 11px;}.close::before, .close::after {height: 21px;}.close::before, .delete::before {transform: rotate(-45deg);}.close::after, .delete::after {transform: rotate(45deg);} .close {height: 30px;width: 30px;margin: 0 0 10px;} input, button {display: block;} button {position: relative; border: 0;background: transparent;} button span {position: absolute; width: 0; height: 0; overflow: hidden; display: block; z-index: -1;} input {border: 0;background: transparent;width: 100%; padding: 10px 40px 10px 10px; margin: 0 0 8px;font-weight: bold;} input + input {background:#fff;box-shadow: 1px 1px 1px rgba(0, 0, 0, .2);font-weight: normal;} .item {background: #efefef;padding: 10px;position: relative; margin: 0 0 70px;} .plus,.delete {position: absolute;} .plus {border: 1px solid #aaa; border-radius: 80px; height: 39px; width: 39px; background: #fff;margin: 10px auto 0; left: 0; right: 0; top: 100%;}.plus::after {transform: rotate(90deg);} .delete {border: 0;background: transparent;right: 10px;top: 10px; width: 30px; height: 30px;} .item:nth-child(3):nth-last-child(3) .delete {display: none;} .resultWrap {margin: 10px 0 0;}.result {border: 1px solid #ccc; background: #333; color: #fff;}.concat {background: #04b8c3;color: #fff;padding: 4px 12px;border-radius: 5px;}.resultWrap span {display: block;color: #398700;font-weight: bold;text-align: center;} .resultWrap  [aria-hidden="true"] {opacity: 0;transition: .2s opacity ease-out;}';
+
+            fragment.append(tasks.getFormSet());
+
+            concatBtn.textContent = '結合する';
+            concatBtn.type = 'button';
+            concatBtn.className = 'concat';
+            concatBtn.addEventListener('click', tasks.concat);
+            fragment.append(concatBtn);
+
+            concatResponse.textContent = '結合しました！';
+            concatResponse.setAttribute('aria-hidden', 'true');
+
+            resultWrap.className = 'resultWrap';
+            resultInput.className = 'result';
+            resultInput.placeholder = '結合結果';
+            resultInput.setAttribute('aria-label', '結合結果');
+            resultInput.readOnly = true;
+            resultWrap.append(resultInput);
+            resultWrap.append(concatResponse);
+            fragment.append(resultWrap);
+
+            return fragment;
+        })();
+
+
+        shadowRoot.append(content);
+
+        doc.body.append(CONCAT_BOOKMARKLETS);
+    };
+
+    if (win.CONCAT_BOOKMARKLETS) {
+        doc.body.append(win.CONCAT_BOOKMARKLETS);
+
+        return;
+    }
+
+    console.group('Concat bookmarklets 1.0.0 2018/12/27: How to use.');
+    console.log('表示されるパネルの中に結合したいブックマークレットを追加していきます。');
+    console.log('結合ボタンを押下することで、対象のブックマークレットを結合することができます。');
+    console.log('結合したブックマークレットの実行内容は、結合したブックマークレットの実行後に`CONCAT_BOOKMARKLETS.executed`から確認することができます。');
+    console.log('GitHub： https://github.com/Soten-Bluesky/bookmarklets/tree/master/複数のブックマークレットを結合する.js');
+
+    win.CONCAT_BOOKMARKLETS = doc.createElement('concat-bookmarklets');
+    init();
+})(window, document);
